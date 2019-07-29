dae077c4d7d053b1fda6d898bf424ef1396e80a3
PiyushSaini12 <200395812@student.georgianc.on.ca>
2019-07-28 20:07:32 -0400

    Second Commit

--- BuyOnline-Assignment/Models/Data/DefaultConnection.cs
+++ BuyOnline-Assignment/Models/Data/DefaultConnection.cs
@@ -10,9 +10,9 @@ namespace BuyOnline_Assignment.Models.Data
     {
         public DbSet<PageDetail> Pages { get; set; }
         public DbSet<SidebarDTO> Sidebar { get; set; }
-        /*public DbSet<CategoryDTO> Categories { get; set; }
+        public DbSet<CategoryDTO> Categories { get; set; }
         public DbSet<ProductDTO> Products { get; set; }
-        public DbSet<UserDTO> Users { get; set; }
+        /*public DbSet<UserDTO> Users { get; set; }
         public DbSet<RoleDTO> Roles { get; set; }
         public DbSet<UserRoleDTO> UserRoles { get; set; }
         public DbSet<OrderDTO> Orders { get; set; }
