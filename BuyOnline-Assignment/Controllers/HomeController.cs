3c3246872183052f806d5e95690aa0570c6e3516
PiyushSaini12 <200395812@student.georgianc.on.ca>
2019-07-29 15:37:20 -0400

    5th commit

---
+++ BuyOnline-Assignment/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BuyOnline_Assignment.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
